4cbd4609115df3a42b5ae9e3da08b6568b79cb5d
mike cohen <mcohen113@msn.com>
2016-10-27 19:20:41 -0400

    heroku test2

--- model/search.js
+++ model/search.js
@@ -1,39 +1,26 @@
-const { MongoClient } = require('mongodb');
-// const { ObjectId } =
-const dbConnection = 'mongodb://localhost:27017/savedholidays';
+// const { MongoClient } = require('mongodb');
+// // const { ObjectId } =
+// const dbConnection = 'mongodb://localhost:27017/savedholidays';
 
 
 
-function searchHolidays (req, res, next) {
-  MongoClient.connect(dbConnection, (err, db) => {
-    if (err) return next(err);
+// function searchHolidays (req, res, next) {
+//   MongoClient.connect(dbConnection, (err, db) => {
+//     if (err) return next(err);
 
-    db.collection('holidays')
-      .find()                 //pulls data from the mongodb
-      .toArray((arrayError, data) => { //sorts data into array
-        if (arrayError) return next(arrayError);
-
-        res.allHebCal = data;
-        db.close();
-        return next();
-
-      });
-  });
-}
-
-// db.collection('holidays')
-//       .find({id:param})
-//       .toArray((arrayError, data) => {
+//     db.collection('holidays')
+//       .find()                 //pulls data from the mongodb
+//       .toArray((arrayError, data) => { //sorts data into array
 //         if (arrayError) return next(arrayError);
-//         console.log(data)
-//         // return the data
-//         res.holidays = data;
+
+//         res.allHebCal = data;
 //         db.close();
 //         return next();
+
 //       });
-//     return false;
+//   });
+// }
 
-  // return false;
 
 
-module.exports = { searchHolidays };
+// module.exports = { searchHolidays };
