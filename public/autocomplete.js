0f6128687755b29c05ea7a378d2ffa0341229133
mike cohen <mcohen113@msn.com>
2017-10-14 18:58:35 -0400

    typeahead holidays

--- public/autocomplete.js
+++ public/autocomplete.js
@@ -3,5 +3,38 @@ var holidayJson = [{
   },
   {
   "name": "sukkot"
+  },
+  {
+  "name": "hanukkah"
+  },
+  {
+  "name": "simchat torah"
+  },
+  {
+  "name": "yom kippur"
+  },
+  {
+  "name": "tu bishvat"
+  },
+  {
+  "name": "pesach"
+  },
+  {
+  "name": "rosh hashanah"
+  },
+  {
+  "name": "shmini atzeret"
+  },
+  {
+  "name": "shavuot"
+  },
+  {
+  "name": "tish'a B'av"
+  },
+  {
+  "name": "lag baomer"
+  },
+  {
+  "name": "yom hashoah"
   }
 ];
